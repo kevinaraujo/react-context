b027af6d9816a1d26b9535f1fd9b29797c5a102a
Kevin Araujo <kevinaraujo@Kevin-Araujos-MacBook-Pro-ArcTouch-447A.local>
2022-12-02 21:59:06 -0300

    feat: Removed UsuarioProvider from router and added to Usuario context file.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -11,55 +11,49 @@ import {
 } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
 
 function Login() {
   const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
 
   return (
     <Container>
-      <UsuarioContext.Consumer>
-        {({ nome, setNome, saldo, setSaldo }) => {
-          return (
-            <>
-              <Titulo>
-                Insira o seu nome
-              </Titulo>
-              <InputContainer>
-                <InputLabel>
-                  Nome
-                </InputLabel>
-                <Input
-                  type="text"
-                  value={nome}
-                  onChange={(e) => setNome(e.target.value)}
-                />
-              </InputContainer>
-              <InputContainer>
-                <InputLabel>
-                  Saldo
-                </InputLabel>
-                <Input
-                  type="number"
-                  value={saldo}
-                  onChange={(e) => setSaldo(e.target.value)}
-                  startAdornment={
-                    <InputAdornment position="start">
-                      R$
-                    </InputAdornment>
-                  }
-                />
-              </InputContainer>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => { history.push('/feira') }}
-              >
-                Avançar
-              </Button>
-            </>
-          )
-        }}
-      </UsuarioContext.Consumer>
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
+          type="number"
+          value={saldo}
+          onChange={(e) => setSaldo(e.target.value)}
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => { history.push('/feira') }}
+      >
+        Avançar
+      </Button>
     </Container>
   )
 };
